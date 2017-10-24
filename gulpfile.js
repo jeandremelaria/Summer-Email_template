aa59a6bb5506b1e023b805d12aa4cf482eed3985
Jeandre Melaria <j.melaria@hotmail.com>
2017-10-24 20:38:41 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+//----- CONSTANTS -----//
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync');
+
+
+//----- LOG MESSAGE -----//
+gulp.task('message', function(){
+	return console.log('Gulp is running...');
+}); // To run type 'gulp message' in command line
+
+//----- COPY ALL HTML FILES TO BUILD FOLDER -----//
+gulp.task('copyHtml', function(){
+	gulp.src('src/*.html')
+	.pipe(gulp.dest('build'));
+})
+
+//----- GULP PREFIXER AND CSS NANO -----//
+gulp.task('prefixerAndCssNano', function(){
+    gulp.src('src/css/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+		.pipe(cssnano())
+        .pipe(gulp.dest('build/css'))
+});
+
+//----- OPTIMIZE IMAGES -----//
+gulp.task('imageMin', function(){
+	gulp.src('src/img/*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('build/img'));
+});
+
+//----- BROWSER SYNC -----//
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'build' //build folder
+        }
+    });
+});
+
+//----- RUN MULTIPLE TASKS AT ONCE IN AN ARRAY -----//
+gulp.task('default', ['message', 'copyHtml', 'imageMin', 'prefixerAndCssNano']);
+
+//----- GULP WATCH -----//
+gulp.task('watch',['browserSync'], function(){
+	gulp.watch('src/css/*.css',['prefixerAndCssNano']);
+	gulp.watch('src/images/*',['imageMin']);
+	gulp.watch('src/*.html',['copyHtml']);
+	
+	//reload browser when the files change
+	gulp.watch('build/css/*.css', browserSync.reload);
+	gulp.watch('build/*.html', browserSync.reload);
+ 
+});
+ 
